dad269ecbce076780651fe9ff4dce01877bb0ebb
Valery Akhrymchuk <valeryakhrymchuk@gmail.com>
2020-04-20 21:25:53 +0300

    Add configurable list of devices

--- src/main/java/org/smarthata/service/health/DeviceHealthCheckService.java
+++ src/main/java/org/smarthata/service/health/DeviceHealthCheckService.java
@@ -6,12 +6,16 @@ import org.smarthata.service.message.EndpointType;
 import org.smarthata.service.message.SmarthataMessage;
 import org.smarthata.service.message.SmarthataMessageBroker;
 import org.smarthata.service.message.SmarthataMessageListener;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static java.time.LocalDateTime.now;
 import static org.smarthata.service.message.EndpointType.SYSTEM;
@@ -22,24 +26,27 @@ public class DeviceHealthCheckService implements SmarthataMessageListener {
 
     private static final Logger LOG = LoggerFactory.getLogger(DeviceHealthCheckService.class);
 
-    private static final List<String> DEVICES = Arrays.asList("/bedroom", "/bathroom", "/heating/floor", "/hum");
     private static final Duration OFFLINE_DURATION = Duration.ofMinutes(30);
-    private static final Duration NOTIFICATION_DURATION = Duration.ofHours(3);
+    private static final Duration NOTIFICATION_DURATION = Duration.ofHours(6);
 
     private final SmarthataMessageBroker messageBroker;
-    private final Map<String, DeviceHealth> deviceTimeMap = createMap();
+    private final Map<String, DeviceHealth> deviceTimeMap;
+    private final List<String> devices;
 
-    private Map<String, DeviceHealth> createMap() {
+    public DeviceHealthCheckService(SmarthataMessageBroker messageBroker, @Value("${health.devices}") List<String> devices) {
+        this.messageBroker = messageBroker;
+        messageBroker.register(this);
+
+        this.devices = devices;
+        deviceTimeMap = createMap(devices);
+    }
+
+    private static Map<String, DeviceHealth> createMap(List<String> devices) {
         Map<String, DeviceHealth> map = new HashMap<>();
-        for (String device : DEVICES) {
+        for (String device : devices) {
             map.put(device, new DeviceHealth(device));
         }
         return map;
-    }
-
-    public DeviceHealthCheckService(SmarthataMessageBroker messageBroker) {
-        this.messageBroker = messageBroker;
-        messageBroker.register(this);
     }
 
 
@@ -72,7 +79,7 @@ public class DeviceHealthCheckService implements SmarthataMessageListener {
 
     private List<DeviceHealth> getOfflineDevices() {
         List<DeviceHealth> offlineDevices = new ArrayList<>();
-        for (String device : DEVICES) {
+        for (String device : devices) {
             DeviceHealth deviceHealth = deviceTimeMap.get(device);
             if (isDateAfter(deviceHealth.getUpdateTime(), OFFLINE_DURATION)) {
                 deviceHealth.setStatus(DeviceStatus.OFFLINE);
