0e74cc05de1ac74d9c461e46c9e111dcebcd7c3d
Valery Akhrymchuk <valeryakhrymchuk@gmail.com>
2022-06-18 23:07:45 +0300

    Do not run swagger on prod

--- src/main/java/org/smarthata/config/SwaggerConfig.java
+++ src/main/java/org/smarthata/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package org.smarthata.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -14,7 +16,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.util.Collections;
 
 @Configuration
+@EnableWebMvc
 @EnableSwagger2
+@Profile({"!prod"})
 public class SwaggerConfig {
 
     @Value("${build.version}")
