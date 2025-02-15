5493a36a8d595a874836957e90f1fa92381d8ca3
Valery Akhrymchuk <valeryakhrymchuk@gmail.com>
2025-02-15 15:54:55 +0300

    Update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 
 plugins {
-    id("org.springframework.boot") version "3.2.3"
-    id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.spring") version "1.9.22"
-    kotlin("plugin.jpa") version "1.9.22"
-    id("net.researchgate.release") version "3.0.2"
+    id("org.springframework.boot") version "3.4.2"
+    id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm") version "2.1.10"
+    kotlin("plugin.spring") version "2.1.10"
+    kotlin("plugin.jpa") version "2.1.10"
+    id("net.researchgate.release") version "3.1.0"
+    id("com.github.ben-manes.versions") version "0.52.0"
 }
 
 repositories {
@@ -27,8 +28,8 @@ dependencies {
     api("javax.xml.bind:jaxb-api:2.3.1")
     api("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
     api("org.springframework.boot:spring-boot-starter-data-jpa")
-    api("org.telegram:telegrambots:6.5.0")
-    runtimeOnly("com.mysql:mysql-connector-j:8.2.0")
+    api("org.telegram:telegrambots:6.9.7.1")
+    runtimeOnly("com.mysql:mysql-connector-j:9.2.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
